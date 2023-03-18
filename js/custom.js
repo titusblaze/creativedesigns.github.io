c8672bdf24f2d588ff44cb20c4fc523840b2bc4e
titusblaze <titus.blaze@gmail.com>
2023-03-18 15:47:55 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -39,3 +39,18 @@ function Move(){
 }
 setInterval(Move,5000); //change img in 3 sec
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
