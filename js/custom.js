29104af16908809b1351d0af06e446ea1d25f63f
titusblaze <titus.blaze@gmail.com>
2023-03-20 08:15:48 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -25,20 +25,25 @@
   }
 
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
 }
 
-window.addEventListener('click',function(e){
-    let x = document.getElementsByClassName('clickbox');
-    if(!x.contains(e.target)){
-        x.style.display = none;
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
     }
-    
-})
+  }
+}
 
 // automatic slider
 onload  = start;
